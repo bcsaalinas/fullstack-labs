088cd66becd6b2444b6704f62317eae59c1bd27a
beto <betocnos2006@gmail.com>
2025-11-21 01:15:22 -0600

    the classic drum kit exercise

--- JavaScript/DOM-Manipulation/DrumKit/index.js
+++ JavaScript/DOM-Manipulation/DrumKit/index.js
@@ -1,45 +1,47 @@
-var numberOfButtons = document.querySelectorAll(".drum").length;
+let numberButtons = document.querySelectorAll(".drum").length;
+let array = document.querySelectorAll(".drum");
 
-for (let i = 0; i < numberOfButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let buttonKey = this.innerHTML;
-    switch (buttonKey) {
-      case "w":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "a":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      case "s":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "d":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "j":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "k":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "l":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      default:
-        console.log(buttonKey.innerHTML);
-    }
+for (let i = 0; i < numberButtons; i++) {
+  array[i].addEventListener("click", function () {
+    const button = this.innerHTML;
+    makeSound(button);
+    buttonAnimation(button);
   });
 }
 
-document.addEventListener("keydown", function () {
-  alert("key was pressed ");
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
+
+const keySoundMap = {
+  w: "sounds/crash.mp3",
+  a: "sounds/kick-bass.mp3",
+  s: "sounds/snare.mp3",
+  d: "sounds/tom-1.mp3",
+  j: "sounds/tom-2.mp3",
+  k: "sounds/tom-3.mp3",
+  l: "sounds/tom-4.mp3",
+};
+
+function makeSound(key) {
+  const audioPath = keySoundMap[key];
+
+  if (!audioPath) {
+    console.log(key);
+    return;
+  }
+
+  const drumSound = new Audio(audioPath);
+  drumSound.play();
+}
+
+function buttonAnimation(currentKey) {
+  let keyPressed = document.querySelector("." + currentKey);
+  keyPressed.classList.add("pressed");
+  console.log(keyPressed.classList);
+
+  setTimeout(function () {
+    keyPressed.classList.remove("pressed");
+  }, 100);
+}
