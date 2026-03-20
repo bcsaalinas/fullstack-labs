bf93d596fc8cfe23afd2d5a6f10543886274871c
bcsaalinas <betocnos2006@gmail.com>
2026-03-20 01:05:26 -0600

    feat: initialize InputHandling project with React and TypeScript
    - Add package.json with dependencies and scripts for development
    - Create App.css for styling the application
    - Implement App.tsx with form handling for user input
    - Include Ethereum logo SVG asset
    - Set global styles in index.css
    - Create main.tsx for rendering the application
    - Configure TypeScript with tsconfig files for app and node
    - Set up Vite configuration for the project

---
+++ React-TypeScript/State/InputHandling/src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import "./App.css";
+import ethereumLogo from "./assets/ethereum-logo-svgrepo-com.svg";
+
+export default function App() {
+  const [fullname, setFullName] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  function changeInput(e: any) {
+    //getting the name form the form and the value, only the cu
+    const eventName = e.target.name;
+    const eventValue = e.target.value;
+
+    console.log(eventName);
+
+    //we want to update only key that changed, and leave the other keys values as they were before
+
+    //prev is our previous state react passes into the updater when its about to calculate the next one
+    setFullName((prev) => {
+      //manually copy everything
+
+      let newState = {
+        firstName: prev.firstName,
+        lastName: prev.lastName,
+        email: prev.email,
+      };
+
+      //update only the input that changed, we can determine this with the inputs name attribute
+      if (eventName === "firstName") {
+        newState.firstName = eventValue;
+      } else if (eventName === "lastName") {
+        newState.lastName = eventValue;
+      } else if (eventName === "email") {
+        newState.email = eventValue;
+      }
+
+      return newState;
+    });
+  }
+
+  return (
+    <div className="form-container">
+      <div className="card">
+        <img src={ethereumLogo} width={"100px"} alt="Etherium logo " />
+        <form action="#">
+          <h1>
+            Hello Again!{fullname.firstName} {fullname.lastName}{" "}
+          </h1>
+          <p>{fullname.email}</p>
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            onChange={changeInput}
+          />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            onChange={changeInput}
+          />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={changeInput}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
